ed15603b587f2d678475f08d21ec9aa5a7d2dd7c
vins-stha <shrs_samar@yahoo.com>
2022-05-11 22:26:24 +0300

    error fixed

--- code/src/Controller/NoteController.php
+++ code/src/Controller/NoteController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Note;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
